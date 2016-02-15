6c72fd107e712cb83acf0857ffe0048c918cbffd
liveworldstrike <lazodellamas12@gmail.com>
2016-02-15 12:19:59 +0100

    añadidos los dos metodos nuevos para saber los numeros de personas que se han dado de alta y los numeros de personas que se han dado de baja

--- Club.java
+++ Club.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 /**
  * Store details of club memberships.
  * 
@@ -8,14 +9,15 @@ import java.util.ArrayList;
 public class Club
 {
     // Define any necessary fields here ...
-    private ArrayList<Membership> mienbros;
+    private ArrayList<Membership> miembros;
     /**
      * Constructor for objects of class Club
      */
     public Club()
     {
         // Initialise any fields here ...
-        mienbros = new ArrayList<Membership>();
+        miembros = new ArrayList<Membership>();
+        int[]mienbrosEnMes = new int[12];
     }
 
     /**
@@ -24,7 +26,7 @@ public class Club
      */
     public void join(Membership member)
     {
-        mienbros.add(member);
+        miembros.add(member);
     }
 
     /**
@@ -33,6 +35,61 @@ public class Club
      */
     public int numberOfMembers()
     {
-        return mienbros.size();
+        return miembros.size();
+    }
+
+    /** 
+     * Calcula el numero de socios que se dieron de alta en un mes determinado. 
+     * El año no nos importa. En caso de que el parametro contenga un valor 
+     * no valido se muestra por pantalla el error.
+     * @param month El mes en el que estamos interesados
+     * @return El numero de socios que se dieron de alta dicho mes
+     */
+    public int joinedMonth(int month )
+    {
+        int contador = 0;
+        if (month < 1 || month > 12){
+            System.out.println("error mes no compatible introducir un valor entre 1-12");
+            contador = -1;
+        }
+        else {
+
+            for (Membership member : miembros){
+                if (member.getMonth()== month){
+                    contador ++;
+                }
+
+            }
+
+        }
+        return contador;
+    }
+
+    /** 
+     * Todos los socios que se han dado de alta un determinado mes de un determinado año se
+     * dan de baja. En caso de que el parametro month contenga un valor no valido se muestra 
+     * por pantalla el error.
+     * @param month El mes en el que estamos interesados
+     * @param year El año en el que estamos interesados
+     * @return Una coleccion con los socios que se han dado de baja del club
+     */
+    public ArrayList<Membership> purge (int month , int year)
+    {
+        ArrayList<Membership> dadosDeBaja = null;
+        if (month >= 1 && month <= 12) {
+            dadosDeBaja = new ArrayList<>();
+            Iterator<Membership> it = miembros.iterator();
+            while (it.hasNext()) {
+                Membership miembro = it.next();
+                if (miembro.getMonth() == month && miembro.getYear() == year) {
+                    miembros.remove(miembro);
+                    dadosDeBaja.add(miembro);
+                }
+            }
+        }
+        else {
+            System.out.println("datos mal introducidos");
+        }
+        return dadosDeBaja;
     }
 }
